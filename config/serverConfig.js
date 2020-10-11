4f324f902cfe0055ff3e7433ca6e8715a78ce089
Sourav Rao <souravrao3110@gmail.com>
2020-10-11 16:52:34 +0530

    database

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -3,7 +3,7 @@ module.exports = {
     // mongoDB URL
     MONGODB_URL:
         process.env.MONGODBURL ||
-        "mongodb://localhost:8000/event-schedule-server",
+        "mongodb://localhost:27017/event-schedule-server",
      //
     // regular database clean up interval
     REGULAR_CLEAN_INTERVAL: 1000 * 60 * 60 * 24 * 7,
